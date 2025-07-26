6d0d93b736f4d4c7efe0dd39bed7e8fa53c8b884
octagonemusic <octagonemusic69@gmail.com>
2025-07-26 08:43:36 +0530

    Solved on 26/07/2025

---
+++ 03_arrays/189.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  void rotate(vector<int> &nums, int k) {
+    int size = nums.size();
+    k = k % size;
+
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+  }
+};
