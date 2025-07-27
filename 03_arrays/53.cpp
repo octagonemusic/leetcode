c80dd3f71af21811532df897877a925ff37fccc1
octagonemusic <octagonemusic69@gmail.com>
2025-07-27 09:20:25 +0530

    Solved on 27/07/2025

---
+++ 03_arrays/53.cpp
@@ -0,0 +1,21 @@
+#include <climits>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int maxSubArray(vector<int> &nums) {
+    int res = nums[0];
+    int currSum = nums[0];
+    if (nums.size() == 1) {
+      return nums[0];
+    }
+    for (int i = 1; i < nums.size(); i++) {
+      currSum = max(nums[i], currSum + nums[i]);
+      res = max(currSum, res);
+    }
+
+    return max(currSum, res);
+  }
+};
