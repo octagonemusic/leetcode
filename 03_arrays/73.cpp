eeb7cf617612e079c95ec7b504811b2acd2be6e6
octagonemusic <octagonemusic69@gmail.com>
2025-07-27 21:17:07 +0530

    Solved on 27/07/2025

--- 03_arrays/73.cpp
+++ 03_arrays/73.cpp
@@ -5,29 +5,25 @@ using namespace std;
 class Solution {
 public:
   void setZeroes(vector<vector<int>> &matrix) {
-    vector<int> markedRows;
-    vector<int> markedColumns;
-
     int rows = matrix.size();
     int cols = matrix[0].size();
+
+    vector<bool> markedRows(rows, false);
+    vector<bool> markedColumns(cols, false);
 
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
         if (matrix[i][j] == 0) {
-          markedRows.push_back(i);
-          markedColumns.push_back(j);
+          markedRows[i] = true;
+          markedColumns[j] = true;
         }
       }
     }
-
-    for (int markedRow : markedRows) {
+    for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
-        matrix[markedRow][j] = 0;
-      }
-    }
-    for (int markedCol : markedColumns) {
-      for (int i = 0; i < rows; i++) {
-        matrix[i][markedCol] = 0;
+        if (markedRows[i] || markedColumns[j]) {
+          matrix[i][j] = 0;
+        }
       }
     }
   }
