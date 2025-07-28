4a7e264344e2cc77e4c3953845ab90eb36041194
octagonemusic <octagonemusic69@gmail.com>
2025-07-28 08:48:02 +0530

    Solved on 28/07/2025

--- 03_arrays/73.cpp
+++ 03_arrays/73.cpp
@@ -8,22 +8,49 @@ public:
     int rows = matrix.size();
     int cols = matrix[0].size();
 
-    vector<bool> markedRows(rows, false);
-    vector<bool> markedColumns(cols, false);
+    bool firstRowHasZero = false;
+    bool firstColHasZero = false;
+
+    for (int j = 0; j < cols; j++) {
+      if (matrix[0][j] == 0) {
+        firstRowHasZero = true;
+        break;
+      }
+    }
 
     for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
+      if (matrix[i][0] == 0) {
+        firstColHasZero = true;
+        break;
+      }
+    }
+
+    for (int i = 1; i < rows; i++) {
+      for (int j = 1; j < cols; j++) {
         if (matrix[i][j] == 0) {
-          markedRows[i] = true;
-          markedColumns[j] = true;
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
         }
       }
     }
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
-        if (markedRows[i] || markedColumns[j]) {
+
+    for (int i = 1; i < rows; i++) {
+      for (int j = 1; j < cols; j++) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
           matrix[i][j] = 0;
         }
+      }
+    }
+
+    if (firstRowHasZero) {
+      for (int j = 0; j < cols; j++) {
+        matrix[0][j] = 0;
+      }
+    }
+
+    if (firstColHasZero) {
+      for (int i = 0; i < rows; i++) {
+        matrix[i][0] = 0;
       }
     }
   }
