cee1f83fface014ee6cf0d8e430e8f5ff585b446
octagonemusic <octagonemusic69@gmail.com>
2025-08-01 18:02:56 +0530

    Solved on 01/08/2025

--- 04_binary_search/34.cpp
+++ 04_binary_search/34.cpp
@@ -4,27 +4,43 @@ using namespace std;
 
 class Solution {
 public:
-  vector<int> searchRange(vector<int> &nums, int target) {
+  int findLeft(vector<int> &nums, int target) {
     int left = 0;
     int right = nums.size() - 1;
+    int leftpos = -1;
     while (left <= right) {
       int mid = (left + right) / 2;
       if (nums[mid] == target) {
-        int startpos = mid;
-        int endpos = mid;
-        while (startpos >= 0 && nums[startpos] == target) {
-          startpos--;
-        }
-        while (endpos < nums.size() && nums[endpos] == target) {
-          endpos++;
-        }
-        return {startpos + 1, endpos - 1};
+        leftpos = mid;
+        right = mid - 1;
       } else if (target < nums[mid]) {
         right = mid - 1;
       } else {
         left = mid + 1;
       }
     }
-    return {-1, -1};
+    return leftpos;
+  }
+
+  int findRight(vector<int> &nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+    int rightPos = -1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (nums[mid] == target) {
+        rightPos = mid;
+        left = mid + 1;
+      } else if (target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return rightPos;
+  }
+
+  vector<int> searchRange(vector<int> &nums, int target) {
+    return {findLeft(nums, target), findRight(nums, target)};
   }
 };
